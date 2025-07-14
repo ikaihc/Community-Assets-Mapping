59f9fddaa39a7a76375070dfb55f5de4779ed8c4
ZhuyuYang46 <156232773+ZhuyuYang46@users.noreply.github.com>
2025-07-14 15:10:44 -0400

    view-asset-admin

--- frontend/asset-mapping-project/src/app/app.routes.ts
+++ frontend/asset-mapping-project/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { ViewAssetComponent } from './pages/view-asset/view-asset.component';
+import { ViewAssetAdminComponent } from './pages/view-asset-admin/view-asset-admin.component';
 
 export const routes: Routes = [
   { path: 'view-asset', component: ViewAssetComponent },
-  { path: '', redirectTo: 'view-asset', pathMatch: 'full' }
+  { path: 'view-asset-admin', component: ViewAssetAdminComponent },
+  { path: '**', redirectTo: 'view-asset' }
 ];
