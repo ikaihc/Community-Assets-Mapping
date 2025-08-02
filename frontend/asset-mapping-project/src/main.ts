03497bdd694f1f01f1cc596412aa2bfcb372e8df
Ernest San Luis <sanl0004@algonquinlive.com>
2025-08-02 13:48:39 -0400

    Home page update (#19)
    * add HomeComponent
    
    * update footer add links
    
    * Add map
    
    * google map components
    
    * styles and pins
    
    * remove leaflet
    
    * add footer component
    
    * modify search bar
    
    * home components update
    
    * scss update for home
    
    * scss
    
    * home component update
    
    ---------
    
    Co-authored-by: ikaihc <114277794+ikaihc@users.noreply.github.com>

--- frontend/asset-mapping-project/src/main.ts
+++ frontend/asset-mapping-project/src/main.ts
@@ -4,16 +4,14 @@ import { provideRouter } from '@angular/router';
 import { importProvidersFrom } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { routes } from './app/app.routes';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'; 
 
 const appConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule), provideAnimationsAsync(),
-    importProvidersFrom(BrowserAnimationsModule) 
-
+    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(BrowserAnimationsModule), provideAnimationsAsync() 
   ]
 };
 
